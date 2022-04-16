829fa2959041dd1a39042ed3a2c9b097c40e123b
malatya44 <malatya44@gmail.com>
2022-04-16 03:42:47 +0300

    Cari Düzenleme

--- MaviOnline/MaviOnline/Controllers/CariPanelController.cs
+++ MaviOnline/MaviOnline/Controllers/CariPanelController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MaviOnline.Models.Sinif;
 namespace MaviOnline.Controllers
 {
@@ -15,11 +16,23 @@ namespace MaviOnline.Controllers
         {
             //sessionda carimailden gelen değeri aldım
             var mail = (string)Session["CariMail"];
-            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            var degerler = c.Carilers.Where(x => x.CariMail == mail).ToList();
             ViewBag.m = mail;
-
+            //Caride Kişinin idisini getirdik
+            var mailid = c.Carilers.Where(x => x.CariMail == mail).Select(y => y.Cariid).FirstOrDefault();
+            ViewBag.mid = mailid;
+            //Personelin Cari Yaptığı Toplam Satis
+            var toplamsatis = c.SatisHarekets.Where(x => x.Cariid == mailid).Count();
+            ViewBag.toplamsatis = toplamsatis;
+            // Carini Toplam Yapılan Satış Ödemeleri
+            var toplamtutar = c.SatisHarekets.Where(x => x.Cariid == mailid).Sum(y => y.ToplamTutar);
+            ViewBag.toplamtutar = toplamtutar;
+            //Carinin Aldığı Ürün Sayısı
+            var toplamurunsayisi = c.SatisHarekets.Where(x => x.Cariid == mailid).Sum(y => y.Adet);
+            ViewBag.toplamurunsayisi = toplamurunsayisi;
             return View(degerler);
         }
+        [Authorize]
         public ActionResult Siparislerim()
         {
             //sessionda carimailden gelen değeri aldım
@@ -31,11 +44,12 @@ namespace MaviOnline.Controllers
             return View(degerler);
         }
         //Mesajlar Kısmı
+        [Authorize]
         public ActionResult GelenMesajlar()
         {
             var mail = (string)Session["CariMail"];
             //Gelen Mesajları orderby ile idye göre sıraladık
-            var mesajlar = c.Mesajlars.Where(x => x.Alici == mail).OrderByDescending(x=>x.MesajID).ToList();
+            var mesajlar = c.Mesajlars.Where(x => x.Alici == mail).OrderByDescending(x => x.MesajID).ToList();
             //maile gelen mesaj sayisini viewbag d1 ile view e yolladık
             var gelensayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
             ViewBag.d1 = gelensayisi;
@@ -45,6 +59,7 @@ namespace MaviOnline.Controllers
 
             return View(mesajlar);
         }
+        [Authorize]
         public ActionResult GidenMesajlar()
         {
             var mail = (string)Session["CariMail"];
@@ -58,6 +73,7 @@ namespace MaviOnline.Controllers
 
             return View(mesajlar);
         }
+        [Authorize]
         public ActionResult MesajDetay(int id)
         {
             var degerler = c.Mesajlars.Where(x => x.MesajID == id).ToList();
@@ -83,6 +99,7 @@ namespace MaviOnline.Controllers
             ViewBag.d2 = gidensayisi;
             return View();
         }
+   
         [HttpPost]
         public ActionResult YeniMesaj(Mesajlar m)
         {
@@ -93,6 +110,29 @@ namespace MaviOnline.Controllers
             c.Mesajlars.Add(m);
             c.SaveChanges();
             return View();
+        }
+        [Authorize]
+        public ActionResult KargoTakip(string p)
+        {
+            var kargo = from x in c.KargoDetays select x;
+
+            kargo = kargo.Where(y => y.TakipKodu.Contains(p));
+
+            return View(kargo.ToList());
+
+        }
+        [Authorize]
+        public ActionResult CariKargoTakip(string id)
+        {
+            var degerler = c.KargoTakips.Where(x => x.TakipKodu == id).ToList();
+            return View(degerler);
+        }
+        [Authorize]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
         }
     }
 }
