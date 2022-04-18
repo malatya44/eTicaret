631f89cab331555d70f86ed9ebfafde7a8712bc3
malatya44 <malatya44@gmail.com>
2022-04-18 05:53:18 +0300

    Dinamik Faturalama Tamam

--- MaviOnline/MaviOnline/Controllers/FaturaController.cs
+++ MaviOnline/MaviOnline/Controllers/FaturaController.cs
@@ -74,5 +74,31 @@ namespace MaviOnline.Controllers
             cs.deger2 = c.FaturaKalems.ToList();
             return View(cs);
         }
+        public ActionResult FaturaKaydet(string FaturaSeriNo,string FaturaSiraNo,DateTime Tarih,string Saat,string VergiDairesi, string TeslimEden,string TeslimAlan,string Toplam, FaturaKalem[] kalemler)
+        {
+            Faturalar f = new Faturalar();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSiraNo = FaturaSiraNo;
+            f.Tarih = Tarih;
+            f.Saat = Saat;
+            f.VergiDairesi = VergiDairesi;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam = decimal.Parse(Toplam);
+            c.Faturalars.Add(f);
+            foreach(var x in kalemler)
+            {
+                FaturaKalem fk = new FaturaKalem();
+                fk.Aciklama = x.Aciklama;
+                fk.BirimFiyat = x.BirimFiyat;
+                fk.Faturaid = x.FaturaKalemid;
+                fk.Miktar = x.Miktar;
+                fk.Tutar = x.Tutar;
+                c.FaturaKalems.Add(fk);
+            }
+            c.SaveChanges();
+            return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
+
+        }
     }
 }
